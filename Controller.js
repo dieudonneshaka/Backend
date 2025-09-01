9493cb40130fbb0bbbc97a920cc6b7e97668ea59
dieudonneshaka <158247669+dieudonneshaka@users.noreply.github.com>
2025-09-01 15:42:46 +0300

    post request and put request are done

---
+++ Controller.js
@@ -0,0 +1,7 @@
+export const userLogin = (req, res) => {
+    res.send('User Login router');
+};
+
+export const userSignup = (req, res) => {
+    res.send('User Signup Router');
+};  
--- server.js
+++ server.js
@@ -1,9 +1,35 @@
-const express = require("express")
+import express from 'express'
+import router from './route.js'
+
 const app = express()
 
-app.get("/", (req, res) => {
-    console.log("dieudonne")
-    res.send("Hello world bora ")
+const port = 3007
+
+//Define a simple route
+app.get('/', (req,res)=>{
+    res.send('hello, Express')
+
 })
 
-app.listen(5001)
+app.use('/user', router)
+
+
+app.post('/users', express.json(), (req, res) => {
+   const { name, email } = req.body;
+   res.json({
+      message: `user ${name} with email ${email} created successfully`
+   });
+});
+
+
+app.put('/users/:id', (req,res)=>{
+    const usersId = req.params.id
+    const { name,email } = req.body
+    res.json({
+        message: `user ${usersId} updated successfully`
+    });
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
